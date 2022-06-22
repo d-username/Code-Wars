4576f483510c087b5f52b70fe294acde2f2a124b
David <czdavid93@gmail.com>
2022-06-23 00:04:16 +0200

    List filtering

--- 7kyu.js
+++ 7kyu.js
@@ -160,3 +160,13 @@ function DNAStrand(dna) {
 // console.log(DNAStrand("GTAT"));
 
 // -----------------------------------------------
+
+// List Filtering
+
+function filter_list(l) {
+  return (numbersOnly = l.filter((element) => Number.isInteger(element)));
+}
+
+// console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
+
+// ------------------------------------------------
