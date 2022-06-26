9b7ff14053e0a4e4314b10d25521e87937f6928f
David <czdavid93@gmail.com>
2022-06-26 18:07:04 +0200

    RGB To Hex Conversion

--- 5kyu.js
+++ 5kyu.js
@@ -36,3 +36,50 @@ function moveZeros(arr) {
 // console.log(moveZeros([false, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
 
 //-------------------------------------------
+
+// RGB To Hex Conversion
+
+function rgb(r, g, b) {
+  chart = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+  };
+
+  let parameters = [r, g, b];
+  let hex = "";
+
+  for (let i = 0; i < parameters.length; i++) {
+    if (parameters[i] < 0) {
+      parameters[i] = 0;
+    }
+    if (parameters[i] > 255) {
+      parameters[i] = 255;
+    }
+  }
+
+  for (let i = 0; i < parameters.length; i++) {
+    let first = Math.floor(parameters[i] / 16);
+    hex += chart[first];
+    let second = (parameters[i] / 16 - first) * 16;
+    hex += chart[second];
+  }
+  return hex;
+}
+
+// console.log(rgb(26, 250, 175));
+
+// ------------------------------------
