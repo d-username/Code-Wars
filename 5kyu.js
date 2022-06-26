d4f1d5bfc80a0592fd7184e32f704d493a34836c
David <czdavid93@gmail.com>
2022-06-26 16:42:59 +0200

    Moving Zeros to The End

--- 5kyu.js
+++ 5kyu.js
@@ -20,3 +20,19 @@ function pigIt(str) {
 }
 
 // console.log(pigIt("Hello world !"));
+
+//Moving Zeros To The End
+
+function moveZeros(arr) {
+  const filtered = arr.filter((element) => element !== 0);
+  const zeros = arr.filter((element) => element === 0).length;
+
+  for (var i = 0; i < zeros; i++) {
+    filtered.push(0);
+  }
+  return filtered;
+}
+
+// console.log(moveZeros([false, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+
+//-------------------------------------------
