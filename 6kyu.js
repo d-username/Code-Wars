08a814dd99c23f782197a2d8bd04d99f027a4740
David <czdavid93@gmail.com>
2022-07-01 00:36:55 +0200

    Persistent Bugger

--- 6kyu.js
+++ 6kyu.js
@@ -401,3 +401,30 @@ function arrayDiff(a, b) {
 // console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
 
 //------------------------------------------
+
+// Persistent Bugger
+
+// Example:
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+function persistence(num) {
+  let count = 0;
+  let digits = num.toString().split('');
+
+  if (digits.length === 1) {
+    return 0;
+  }
+
+  while (digits.length > 1) {
+    let multiplied = digits.reduce((a, b) => a * b, 1);
+    digits = multiplied.toString().split('');
+    count++;
+  }
+  return count;
+}
+
+// console.log(persistence(39));
+
+// ----------------------------------
