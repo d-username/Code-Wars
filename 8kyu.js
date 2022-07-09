2249843a7e62f3df3085ad71c98862bb07c0c76d
David <czdavid93@gmail.com>
2022-07-09 21:54:39 +0200

    Counting Sheep

--- 8kyu.js
+++ 8kyu.js
@@ -19,7 +19,50 @@ function mergeArrays(arr1, arr2) {
 // Fixme - Replace all dots
 
 var replaceDots = function (str) {
-  return (resultingString = str.split(".").join("-"));
+  return (resultingString = str.split('.').join('-'));
 };
 
 //console.log(replaceDots("one.two.three"));
+
+// Counting sheep...
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// console.log(
+//   countSheeps([
+//     true,
+//     true,
+//     true,
+//     false,
+//     true,
+//     true,
+//     true,
+//     true,
+//     true,
+//     false,
+//     true,
+//     false,
+//     true,
+//     false,
+//     false,
+//     true,
+//     true,
+//     true,
+//     true,
+//     true,
+//     false,
+//     false,
+//     true,
+//     true,
+//   ])
+// );
+
+// ------------------------------
